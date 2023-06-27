6db70e807509d58c81d388a284303f5dc2e0aca7
felipe.baidon <felipe.baidon94@gmail.com>
2023-06-27 09:13:56 -0600

    finished implementation of assignment2

--- RealTimeEmbeddedSystems/conceptsAndPractices/A2: MultipleThreads/incdecthread/pthread.c
+++ RealTimeEmbeddedSystems/conceptsAndPractices/A2: MultipleThreads/incdecthread/pthread.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sched.h>
-#define COUNT  10
+#include <syslog.h>
 
 typedef struct
 {
@@ -20,48 +20,33 @@ int gsum=0;
 
 void *incThread(void *threadp)
 {
-    int i;
-    threadParams_t *threadParams = (threadParams_t *)threadp;
+   int i = 0;
 
-    for(i=0; i<COUNT; i++)
-    {
-        gsum=gsum+i;
-        printf("Increment thread idx=%d, gsum=%d\n", threadParams->threadIdx, gsum);
-    }
+   threadParams_t *threadParams = (threadParams_t *)threadp;
+
+    for(i= 0; i < threadParams->threadIdx + 1; i++)
+        gsum= gsum + i;
+    
+    syslog(LOG_CRIT,"[COURSE:1][ASSIGNMENT:2]: Increment thread idx=%d, gsum=%d\n", threadParams->threadIdx, gsum);
+    gsum = 0;
 }
 
-
-void *decThread(void *threadp)
-{
-    int i;
-    threadParams_t *threadParams = (threadParams_t *)threadp;
-
-    for(i=0; i<COUNT; i++)
-    {
-        gsum=gsum-i;
-        printf("Decrement thread idx=%d, gsum=%d\n", threadParams->threadIdx, gsum);
-    }
-}
-
-
-typedef void* (*fnPtr[])(void *);
-fnPtr fnPtrArr = {incThread, decThread};
 
 int main (int argc, char *argv[])
 {
    int rc;
    int i=0;
-   int j = 0;
-
+   
+   system("uname -a | logger");   
+   
    for( i= 0; i< 128; i++)
    {
    threadParams[i].threadIdx=i;
    pthread_create(&threads[i],   // pointer to thread descriptor
                   (void *)0,     // use default attributes
-                  fnPtrArr[j],
+                  incThread,
                   (void *)&(threadParams[i]) // parameters to pass in
                  );
-   j = (j+1)%2;
    }
 
    for(i=0; i<128; i++)
