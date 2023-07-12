4b93a6f3503c525e19651cbaeb17992a37d8be6d
felipe.baidon <felipe.baidon94@gmail.com>
2023-07-12 16:44:01 -0600

    Commented code for Walkthrough

--- RealTimeEmbeddedSystems/conceptsAndPractices/A3_RealTimeThreads/incdecthread/pthread.c
+++ RealTimeEmbeddedSystems/conceptsAndPractices/A3_RealTimeThreads/incdecthread/pthread.c
@@ -1,4 +1,4 @@
-#define _GNU_SOURCE
+#define _GNU_SOURCE   //This definition is a MUST in order for the code to work, it MUST be at the top
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -8,7 +8,7 @@
 #include <unistd.h>
 #include <syslog.h>
 
-#define SCHED_POLICY  SCHED_FIFO
+#define SCHED_POLICY  SCHED_FIFO //Purpose of this assignment is to use SCHED_FIFO policy, we are using a macro definition for convenience 
 #define NUM_THREADS   128
 #define MAX_ITERATIONS 100000000
 
@@ -22,7 +22,8 @@ typedef struct
 //
 pthread_t threads[128];
 threadParams_t threadParams[128];
-pthread_attr_t fifo_sched_attr;
+pthread_attr_t fifo_sched_attr; //This structure contains thread's attributes such as scope, stack size, stack address, scheduling
+                                // policy, scheduling priority, inherit scheduler
 struct sched_param fifo_param;
 
 // Unsafe global
@@ -34,18 +35,20 @@ void *incThread(void *threadp)
     threadParams_t *threadParams = (threadParams_t *)threadp;
 
    //Uncomment to add a delay for testing conveniences
-   // for(iterations = 0; iterations < MAX_ITERATIONS; iterations++)
-    //{
+    for(iterations = 0; iterations < MAX_ITERATIONS; iterations++)
+    {
         gsum= 0;
-        for(i=0; i<threadParams -> threadIdx ; i++)
+        for(i=0; i<threadParams -> threadIdx + 1 ; i++)
         {
             gsum=gsum+i;
         }
-    //}
+    }
     
     syslog(LOG_USER |LOG_DEBUG, "[COURSE:1][ASSIGNMENT:3]: Thread idx=%d, sum[1...%d]=%d Running on core: %d", threadParams->threadIdx, threadParams ->threadIdx, gsum, sched_getcpu());
 }
 
+// This function is used for debug purposes, it helps to know
+//if the scheduling policy has correctly being set 
 void print_scheduler(void)
 {
     int schedType = sched_getscheduler(getpid());
@@ -75,24 +78,32 @@ void set_scheduler(void)
     syslog(LOG_USER|LOG_DEBUG,"INITIAL ");print_scheduler();
 
     //Set policy to SCHED_FIFO
-    pthread_attr_init(&fifo_sched_attr);
-    pthread_attr_setinheritsched(&fifo_sched_attr, PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_init(&fifo_sched_attr); //Used to initialize thread's attributes to default 
+    // This function sets the inehrit scheduler attribute to the argument passed, in this case to explicit scheduling
+    // this means that each time a thread is created it will take its attributes from the fifo_sched_attr structure
+    // instead of inherit them from the calling thread
+    pthread_attr_setinheritsched(&fifo_sched_attr, PTHREAD_EXPLICIT_SCHED); 
+
+    //set the scheduling policy to SCHED_FIFO
     pthread_attr_setschedpolicy( &fifo_sched_attr, SCHED_POLICY);
 
-    //Assign pthread to core 1 (the 0th core) on ubuntu virtual machine
+    //Assign pthread to core 1 on ubuntu virtual machine
     CPU_ZERO(&cpuset);
     cpuidx=(1);
     CPU_SET(cpuidx, &cpuset);
-    
+   
+    //This function will assign the thread to run on the CPU indicated by cpuset 
     pthread_attr_setaffinity_np(&fifo_sched_attr,  sizeof(cpu_set_t), &cpuset); 
     
     //set the RM priority to MAX
    max_priority = sched_get_priority_max(SCHED_POLICY); 
    fifo_param.sched_priority = max_priority;
 
+   //Sets the scheduling policy and priority for the thread whose id is specificated by pid
    if((rc= sched_setscheduler(getpid(), SCHED_POLICY, &fifo_param)) < 0)
        perror("sched_setscheduler");
 
+    //Sets the attributes of fifo_sched_attr to the values specified in fifo_param
     pthread_attr_setschedparam(&fifo_sched_attr, &fifo_param);
     
     syslog(LOG_USER|LOG_DEBUG, "ADJUSTED ");  print_scheduler();
